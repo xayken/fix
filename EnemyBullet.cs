ea579f6c7a4d4eba561060f7724e2fef4920a4a1
xayken <118131828+xayken@users.noreply.github.com>
2023-07-10 17:16:55 +0300

    Add files via upload

---
+++ EnemyBullet.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed = 10f;
+    public float lifetime = 3f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Health playerHealth = collision.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PlayerInventory.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public WeaponData[] weapons;
+    
+}
